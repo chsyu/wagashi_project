e443c437e14f0285bce2ade12844ecb3c5d4d622
Alva <xshinoa0208@gmail.com>
2023-10-19 14:24:54 +0800

    enable onclick(without fade-in)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+window.onload = function() {
+    // 添加一个 CSS 类，以触发淡出和平移效果
+    document.body.classList.add('fade-in');
+
+    // 获取元素
+    var wagashiImages = document.querySelectorAll('.maincotent');
+
+    // 执行平移效果
+    setTimeout(function() {
+        wagashiImages.forEach(function(image) {
+            image.style.marginBottom = '-50px';
+        });
+    }, 500); // 500 毫秒是淡出效果的时间
+
+    // 过渡结束后，移除动画类
+    setTimeout(function() {
+        document.body.classList.remove('fade-out');
+    }, 1000); // 1000 毫秒是淡出和平移效果的总时间
+}
+
 function navigateToSpringPage() {
     // 添加一个 CSS 类，以触发淡出过渡效果
     document.body.classList.add('fade-out');
@@ -166,13 +186,6 @@ menutoprect.onclick = function(){
 //     }
 // }
 
-
-
-
-
 // gasp.fromTo('menubottomrect',1,{},{})
-
-
-
 // 先執行完展開，再visible
 
--- js/spring-animation.js
+++ js/spring-animation.js
@@ -29,8 +29,173 @@ window.onload = function() {
 //     });
 // });
 
-var wagashi_introduction=document.querySelector("wagashi_introduction1");
+// var wagashi_introduction=document.querySelector("wagashi_introduction1");
 
-wagashi_introduction1.onclick = function () {
-    // let wagashi_introduction= wagashi_introduction1.setAttribute
+// wagashi_introduction1.onclick = function () {
+//     // let wagashi_introduction= wagashi_introduction1.setAttribute
+// }
+
+let clickstate = 0;
+let lookNum=0;
+let wagashi1 = document.querySelector("#wagashi1");
+let wagashi2 =document.querySelector("#wagashi2");
+let wagashi3 =document.querySelector("#wagashi3");
+let wagashi4 =document.querySelector("#wagashi4");
+let wagashi5 =document.querySelector("#wagashi5");
+let wagashi6 =document.querySelector("#wagashi6");
+
+let look1 = document.querySelector("#look1");
+let look2 = document.querySelector("#look2");
+let look3 = document.querySelector("#look3");
+let look4 = document.querySelector("#look4");
+let look5 = document.querySelector("#look5");
+let look6 = document.querySelector("#look6");
+
+let btn_left = document.querySelector("#btn_left");
+let btn_right = document.querySelector("#btn_right");
+let btn_close = document.querySelector("#btn_close");
+
+let popup_spring = document.querySelector("#popup_spring");
+
+// function addpopupNum() {
+//     popup_spring.classList.add("#look"+${popupNum[i]});
+// }
+
+
+
+wagashi1.onclick = function() {
+    if(clickstate == 0){
+        // document.popup_spring.classList.add(".fade-in");
+
+
+        popup_spring.style.display = "flex";
+        look1.style.display = "flex";
+        lookNum=1;
+        clickstate = 1;
+    };
+
+};
+wagashi2.onclick = function() {
+    if(clickstate == 0){
+        look2.style.display = "flex";
+        popup_spring.style.display = "flex";
+        
+        lookNum=2;
+        clickstate = 1;
+    };
+};
+wagashi3.onclick = function() {
+    if(clickstate == 0){
+        look6.style.display = "flex";
+        popup_spring.style.display = "flex";
+        
+        lookNum=6;
+        clickstate = 1;
+    };
+};
+wagashi4.onclick = function() {
+    if(clickstate == 0){
+        popup_spring.style.display = "flex";
+        look3.style.display = "flex";
+        lookNum=3;
+        clickstate = 1;
+    };
+};
+wagashi5.onclick = function() {
+    if(clickstate == 0){
+        popup_spring.style.display = "flex";
+        look5.style.display = "flex";
+        lookNum=5;
+        clickstate = 1;
+    };
+};
+wagashi6.onclick = function() {
+    if(clickstate == 0){
+        popup_spring.style.display = "flex";
+        look4.style.display = "flex";
+        lookNum=4;
+        clickstate = 1;
+    };
+};
+
+btn_left.onclick = function(){
+    if(lookNum==1){
+        look1.style.display="none";
+        look6.style.display="flex";
+        lookNum=7;
+    }else if(lookNum==2){
+        look2.style.display="none";
+        look1.style.display="flex";
+    }else if(lookNum==3){
+        look3.style.display="none";
+        look2.style.display="flex";
+    }else if(lookNum==4){
+        look4.style.display="none";
+        look3.style.display="flex";
+    }else if(lookNum==5){
+        look5.style.display="none";
+        look4.style.display="flex";
+    }else{
+        look6.style.display="none";
+        look5.style.display="flex";
+    }
+
+    lookNum=lookNum-1;
+    
+    
 }
+btn_right.onclick = function() {
+    
+    if(lookNum==1){
+        look1.style.display="none";
+        look2.style.display="flex";
+    }else if(lookNum==2){
+        look2.style.display="none";
+        look3.style.display="flex";
+    }else if(lookNum==3){
+        look3.style.display="none";
+        look4.style.display="flex";
+    }else if(lookNum==4){
+        look4.style.display="none";
+        look5.style.display="flex";
+    }else if(lookNum==5){
+        look5.style.display="none";
+        look6.style.display="flex";
+    }else{
+        look6.style.display="none";
+        look1.style.display="flex";
+        lookNum=0;
+    }
+    lookNum=lookNum+1;
+}
+
+btn_close.onclick = function() {
+    // btn_close.style.image="./image/close.png";
+    popup_spring.style.display="none";
+    clickstate=0;
+    look1.style.display = "none";
+    look2.style.display = "none";
+    look3.style.display = "none";
+    look4.style.display = "none";
+    look5.style.display = "none";
+    look6.style.display = "none";
+
+}
+
+
+function navigateToMainPage() {
+    // 添加一个 CSS 类，以触发淡出过渡效果
+    document.body.classList.add('fade-out');
+
+    // 在过渡结束后，页面导航到 spring.html
+    setTimeout(function() {
+        // window.location.href = "spring.html";
+        window.open("index.html", "_blank");
+    }, 500); // 500 毫秒是过渡效果的时间
+}
+
+
+
+
+
+
